f4803dbfd64b2c4d1d4966f6989b2c68c86dd4cb
KevinCua <kevinlexcua@yahoo.com>
2015-03-08 14:06:57 +0800

    added schedules

--- ScheduleViewer/ScheduleViewController.swift
+++ ScheduleViewer/ScheduleViewController.swift
@@ -8,90 +8,161 @@
 
 import UIKit
 
-class ScheduleViewController: UITableViewController {
+class ScheduleViewController: UITableViewController, ScheduleEditorViewControllerDelegate{
 
+    var schedules:[Schedule]
+    
+    required init(coder aDecoder: NSCoder) {
+        self.schedules = [Schedule]()
+        
+        super.init(coder: aDecoder)
+        loadSchedules()
+    }
+    
+    func loadSchedules() {
+        // 1
+        let path = dataFilePath()
+        // 2
+        if NSFileManager.defaultManager().fileExistsAtPath(path) {
+            // 3
+            let data = NSData(contentsOfFile: path)
+            let unarchiver = NSKeyedUnarchiver(forReadingWithData: data!)
+            schedules = unarchiver.decodeObjectForKey("Schedules") as [Schedule]
+            unarchiver.finishDecoding()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Documents folder is \(documentsDirectory())")
+        println("Data file path is \(dataFilePath())")
     }
-
+    
+    
+    func documentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as [String]
+        return paths[0]
+    }
+    
+    func dataFilePath() -> String {
+        return documentsDirectory().stringByAppendingPathComponent("Schedules.plist")
+    }
+    
+    func saveSchedules() {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(schedules, forKey: "Schedules")
+        archiver.finishEncoding()
+        data.writeToFile(dataFilePath(), atomically: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+    
+    
+    @IBAction func add(sender: AnyObject) {
+        let newRowIndex = self.schedules.count
+        
+        let schedule = Schedule()
+        schedule.name = "I am gay."
+        self.schedules.append(schedule)
+        
+        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+        let indexPaths = [indexPath]
+        
+        self.tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
     }
-
+    
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        return self.schedules.count
     }
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("Schedule") as UITableViewCell
+        
+        let schedule = schedules[indexPath.row]
+        
+        configureTextForCell(cell, withSchedule: schedule)
+        //configureCheckmarkForCell(cell, withSchedule: schedule)
+        
         return cell
     }
-    */
-
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            let schedule = schedules[indexPath.row]
+            //Schedule.toggleChecked()
+            
+            //  configureCheckmarkForCell(cell, withChecklistSchedule: Schedule)
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        saveSchedules()
+    }
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    func configureCheckmarkForCell(cell: UITableViewCell, withChecklistSchedule Schedule: ChecklistSchedule) {
+    let label = cell.viewWithTag(1001) as UILabel
+    if Schedule.checked {
+    label.text = "√"
+    } else {
+    label.text = ""
+    }
     }
     */
-
-    /*
-    // Override to support editing the table view.
+    func configureTextForCell(cell: UITableViewCell, withSchedule schedule: Schedule) {
+        let label = cell.viewWithTag(1000) as UILabel
+        label.text = schedule.name
+    }
+    
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        schedules.removeAtIndex(indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+        saveSchedules()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    func scheduleEditorViewControllerDidCancel(controller: ScheduleEditorViewController) {
+        dismissViewControllerAnimated(true, completion: nil);
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    
+    func scheduleEditorViewController(controller: ScheduleEditorViewController, didFinishAddingSchedule schedule: Schedule) {
+        let newRowIndex = schedules.count
+        schedules.append(schedule)
+        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0);
+        let indexPaths = [indexPath]
+        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+        dismissViewControllerAnimated(true, completion: nil)
+        saveSchedules()
+        
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    func scheduleEditorViewController(controller: ScheduleEditorViewController, didFinishEditingSchedule schedule: Schedule) {
+        if let index = find(schedules, schedule) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                configureTextForCell(cell, withSchedule: schedule)
+            }
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+        saveSchedules()
     }
-    */
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        let navigationController = segue.destinationViewController as UINavigationController
+        let controller = navigationController.topViewController as ScheduleEditorViewController
+        controller.delegate = self
+        
+        if segue.identifier == "EditSchedule" {
+            if let indexPath = tableView.indexPathForCell(sender as UITableViewCell) {
+                controller.scheduleToEdit = schedules[indexPath.row]
+            }
+        }
+    }
 }
+
